5e14a8b625f87dc264e8f6ba47176e37c6012926
Josh Balch <Joshrbalch@gmail.com>
2024-11-03 12:54:21 -0600

    init

---
+++ Fall2024-Assignment4-CS330/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Fall2024_Assignment4_CS330.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
