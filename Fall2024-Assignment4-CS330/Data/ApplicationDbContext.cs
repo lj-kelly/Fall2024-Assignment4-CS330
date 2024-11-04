efaf453dfa70c0d874ccf7733cebb67c5b4c6101
Josh Balch <Joshrbalch@gmail.com>
2024-11-04 15:56:54 -0600

    .gitignore integration

---
+++ Fall2024-Assignment4-CS330/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Fall2024_Assignment4_CS330.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
