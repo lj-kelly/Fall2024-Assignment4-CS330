903c1faaf4f2a9859b05e1d1d2f7f215d4ff4f30
Liam Kelly <lkelly3@crimson.ua.edu>
2024-12-05 14:02:35 -0600

    added usertype to model, view, and controller. Added pro view(different colors for gameplay).

--- Fall2024-Assignment4-CS330/Models/ApplicationUser.cs
+++ Fall2024-Assignment4-CS330/Models/ApplicationUser.cs
@@ -3,9 +3,10 @@
 namespace Fall2024_Assignment4_CS330.Models
 {
     public class ApplicationUser : IdentityUser
-    {
+    { 
         public int GamesWon { get; set; }
         public int GamesLost { get; set; }
+        public string UserType { get; set; }
         public byte[]? Photo { get; set; }
     }
 }
