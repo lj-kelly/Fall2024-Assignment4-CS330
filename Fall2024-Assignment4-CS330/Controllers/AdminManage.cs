824d0267258f850e0e8ffce68464794798c0d35f
Liam Kelly <lkelly3@crimson.ua.edu>
2024-12-10 14:11:22 -0600

    admin manage view now validates the user's level and does not allow standard users to view the page. Added tab link to top of pages. Still need to add functionality to change a user's type.

--- Fall2024-Assignment4-CS330/Controllers/AdminManage.cs
+++ Fall2024-Assignment4-CS330/Controllers/AdminManage.cs
@@ -3,6 +3,7 @@ using Fall2024_Assignment4_CS330.Models;
 using Microsoft.AspNetCore.Identity;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Security.Claims;
 
 namespace Fall2024_Assignment4_CS330.Controllers
 {
@@ -18,14 +19,30 @@ namespace Fall2024_Assignment4_CS330.Controllers
         // GET: AdminManage/Index
         public async Task<IActionResult> Index()
         {
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            var user = await _userManager.FindByIdAsync(userId);
+
+            if (user == null)
+            {
+                return RedirectToAction("Denied", "Home");
+            }
+
+            var claims = await _userManager.GetClaimsAsync(user);
+            var userType = claims.FirstOrDefault(c => c.Type == "UserType")?.Value;
+
+            // Check if the user is a "Pro"
+            if (userType != "Pro")
+            {
+                return RedirectToAction("Denied", "Home"); // Redirect to Access Denied page
+            }
             // Fetch all users
             var users = _userManager.Users.ToList();
 
             // Populate the list and sort by usertype then username
             var adminmanageVM = new AdminManageVM
             {
-                Users = users.OrderByDescending(u =>
-                    u.UserType)
+                Users = users
+                    .OrderByDescending(u => u.UserType)
                     .ThenByDescending(u => u.UserName) // Secondary sorting for ties
                     .ToList()
             };
