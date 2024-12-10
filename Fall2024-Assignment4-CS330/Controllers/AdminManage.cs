ed687c85b075d4edcd843633ed6ac794eae63e2e
Liam Kelly <lkelly3@crimson.ua.edu>
2024-12-10 13:37:00 -0600

    implemented admin view. Need to add link/tab to it, check admin status for privileges to access, and allow admin to change usertype of users from page.

---
+++ Fall2024-Assignment4-CS330/Controllers/AdminManage.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Fall2024_Assignment4_CS330.Models;
+using Microsoft.AspNetCore.Identity;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Fall2024_Assignment4_CS330.Controllers
+{
+    public class AdminManageController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public AdminManageController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        // GET: AdminManage/Index
+        public async Task<IActionResult> Index()
+        {
+            // Fetch all users
+            var users = _userManager.Users.ToList();
+
+            // Populate the list and sort by usertype then username
+            var adminmanageVM = new AdminManageVM
+            {
+                Users = users.OrderByDescending(u =>
+                    u.UserType)
+                    .ThenByDescending(u => u.UserName) // Secondary sorting for ties
+                    .ToList()
+            };
+
+            // Pass the model to the view
+            return View(adminmanageVM);
+        }
+    }
+}
---
+++ Fall2024-Assignment4-CS330/Models/AdminManageVM.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.Linq;
+
+namespace Fall2024_Assignment4_CS330.Models
+{
+    public class AdminManageVM
+    {
+        private List<ApplicationUser> _users;
+
+        public List<ApplicationUser> Users
+        {
+            get => _users.OrderByDescending(u => u.UserType).ToList(); // Sort by W/L ratio
+            set => _users = value;
+        }
+        public async Task<AdminManageVM> GetUserListAsync(UserManager<ApplicationUser> userManager)
+        {
+            var users = userManager.Users.ToList(); // Retrieve all users
+            var da_list = new AdminManageVM
+            {
+                Users = users // Sorting happens automatically in the property
+            };
+            return da_list;
+        }
+
+    }
+}
